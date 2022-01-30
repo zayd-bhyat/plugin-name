04a2f203ff6563af1f5b967454f8f13a00f654d2
Zayd Bhyat <zayd_bhyat@hotmail.com>
2022-01-30 18:31:45 +0200

    End Lesson 15

--- admin/partials/plugin-name-yt-importer.php
+++ admin/partials/plugin-name-yt-importer.php
@@ -51,66 +51,129 @@
   </div>
 </div>
 <?php
-//Store key to Variables
-/*
-$theyoutubekey = get_option('youtubeAPIKey');
-$thechannelid = get_option( 'youtubeChannelID');
-$arrContextOptions=array(
-  "ssl"=>array(
-      "verify_peer"=>false,
-      "verify_peer_name"=>false,
-  ),
-);
-//Retrieve list of videos
-//$videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$thechannelid.'&maxResults='.'1'.'&key='.$theyoutubekey.'', false, stream_context_create($arrContextOptions)));
 
-//Sorth through items and output
-foreach($videoList->items as $item){
-  $yt_title=$item->snippet->title;
-  $yt_description=$item->snippet->description;
-  //insert a new post video to CPT
-  $data=array(
-    'post_title' =>  wp_strip_all_tags($yt_title),
-    'post_description' => wp_strip_all_tags($yt_description),
-    'post_category' => array('uncategorized'),
-    'tags_input' => array(1),
-    'post_status' => 'publish',
-    'post_type' => 'plugin-name-ytvids'
-  );
+  //Store key to Variables
+  //the action
+  $theaction = '';
+  
+  //Bool for user import
+  $blnImport = false;//Determine if user imported
+  $blnDelete = false;//Detemine if user deleted
+  //get the action
 
-//insert this post into the DB and retrive the ID
-$result = wp_insert_post($data);
-
-//capture ID of post
-if($result && ! is_wp_error($result)){
-  $thenewpostID = $result;
-
-  //add YT meta data
-  add_post_meta($thenewpostID,'videoID',$item->id);
-  add_post_meta($thenewpostID,'publishedAt',$item->snippet->publishedAt);
-  add_post_meta($thenewpostID,'channelId',$item->snippet->channelId);
-  add_post_meta($thenewpostID,'yt_title',$item->snippet->title);
-  add_post_meta($thenewpostID,'yt_description',$item->snippet->description);
-  add_post_meta( $thenewpostID, 'imageresmed', $item->snippet->thumbnails->medium->url);
-  add_post_meta( $thenewpostID, 'imagereshigh', $item->snippet->thumbnails->high->url);
-  echo('<img src="'. get_post_meta( $thenewpostID, 'imageresmed',true ).'"/>');
-?>
-  <img src ="<?php get_post_meta($thenewpostID, 'imageresmed',true); ?>"/>
-<?php
- }
-?>
-
-
-  <!-- Loop videos and add to CPT -->
-
-  <!--<div>
-    <h1>// echo $item->snippet->title;?></h1>
-    <h3>// echo $item->snippet->description;?></h3>
-    <img src="// echo $item->snippet->thumbnails->medium->url;?>">
-  </div>
-  </br>
-  </br>-->
-  <?php} */
+  if (isset($_GET['action'])){
+    $theaction= $_GET['action'];
+  }
+  
+  //=======================
+  //=======================
+  //Import Action start
 
   
+  if($theaction == 'import'){
+
+  $theyoutubekey = get_option('youtubeAPIKey');
+  $thechannelid = get_option( 'youtubeChannelID');
+  $arrContextOptions=array(
+    "ssl"=>array(
+        "verify_peer"=>false,
+        "verify_peer_name"=>false,
+    ),
+  );
+  //Retrieve list of videos
+  $videoList = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$thechannelid.'&maxResults='.'5'.'&key='.$theyoutubekey.'', false, stream_context_create($arrContextOptions)));
+
+  //Sorth through items and output
+  foreach($videoList->items as $item){
+    $yt_title=$item->snippet->title;
+    $yt_description=$item->snippet->description;
+    //insert a new post video to CPT
+    $data=array(
+      'post_title' =>  wp_strip_all_tags($yt_title),
+      'post_description' => wp_strip_all_tags($yt_description),
+      'post_category' => array('uncategorized'),
+      'tags_input' => array(1),
+      'post_status' => 'publish',
+      'post_type' => 'plugin-name-ytvids'
+    );
+
+  //insert this post into the DB and retrive the ID
+  $result = wp_insert_post($data);
+
+  //capture ID of post
+  if($result && ! is_wp_error($result)){
+    $thenewpostID = $result;
+
+    //add YT meta data
+    add_post_meta($thenewpostID,'videoID',$item->id);
+    add_post_meta($thenewpostID,'publishedAt',$item->snippet->publishedAt);
+    add_post_meta($thenewpostID,'channelId',$item->snippet->channelId);
+    add_post_meta($thenewpostID,'yt_title',$item->snippet->title);
+    add_post_meta($thenewpostID,'yt_description',$item->snippet->description);
+    add_post_meta( $thenewpostID, 'imageresmed', $item->snippet->thumbnails->medium->url);
+    add_post_meta( $thenewpostID, 'imagereshigh', $item->snippet->thumbnails->high->url);
+    //echo('<img src="'. get_post_meta( $thenewpostID, 'imageresmed',true ).'"/>');
+  
+    //Set Import True
+    $blnImport = true;
+    ?>
+      <img src ="<?php //get_post_meta($thenewpostID, 'imageresmed',true); ?>"/>
+    <?php
+    }//endif capture post
+    ?>
+
+
+    <!-- Loop videos and add to CPT -->
+
+    <div>
+      <h1><?php //echo $item->snippet->title;?></h1>
+      <h3><?php //echo $item->snippet->description;?></h3>
+      <img src="<?php // echo $item->snippet->thumbnails->medium->url;?>">
+    </div>
+    </br>
+    </br
+    <?php }//end for each
+
+  }//end import if
+  //end import
+  //=======================
+  //=======================
+
+  //=======================
+  //=======================
+  //Delete Action start
+  if($theaction == 'delete'){
+    //delete all videos of CPT
+
+    //get all the posts
+    $allWPYTPost = get_posts(array('post_type'=>'plugin-name-ytvids'));
+    //Loop through and de;ete all posts
+    foreach($allWPYTPost as $eachYTpost){
+      wp_delete_post($eachYTpost ->ID, true);
+      $blnDelete = true;
+    }
+  }
+  
+  //Delete Action end
+  //=======================
+  //=======================
+  
+  //output results
+  if($blnImport == true){
+    ?>
+
+    <br><br>
+    <div class="alert alert-success" style="max-width:100%;">
+      <h2>You have scuccessfully imported the Videos</h2>
+    </div>
+<?php
+  }
+  elseif($blnDelete == true){
+  ?>
+    <br><br>
+    <div class="alert alert-danger" style="max-width:100%;">
+      <h2>You have scuccessfully deleted the Videos</h2>
+    </div>
+  <?php
+  }
   ?>
